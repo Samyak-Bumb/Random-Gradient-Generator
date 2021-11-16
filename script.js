8ebaae073f8b2e67abd35015a54e70532070f876
Samyak Bumb <vijaybumb0575@gmail.com>
2021-11-16 11:28:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-let generateBtn = document.getElementById("generate");
-let copyBtn = document.getElementById("copy-btn");
-let outputColor = document.getElementById("output-color");
-let outputCode = document.getElementById("output-code");
-let hexString = "0123456789abcdef";
+const generateBtn = document.getElementById("generate"),
+	copyBtn = document.getElementById("copy-btn"),
+	outputColor = document.getElementById("output-color"),
+	outputCode = document.getElementById("output-code");
+	hexString = "0123456789abcdef";
+
 let randomColor = ()=>{
     let hexCode = "#";
-    for( i=0; i<6; i++){
+    for( i = 0; i < 6; i++){
         hexCode += hexString[Math.floor(Math.random() * hexString.length)];
     }
     return hexCode;
 }
 let generateGrad = ()=>{
-    let colorOne = randomColor();
-    let colorTwo = randomColor();
-    let angle = Math.floor(Math.random() * 360);
-    outputColor.style.background = `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
+    let colorOne = randomColor(),
+		colorTwo = randomColor(),
+		angle = Math.floor(Math.random() * 360);
+	outputColor.style.background = `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
     outputCode.value = `background: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo});`;
 }
-copyBtn.addEventListener("click", ()=>{
+
+copyBtn.addEventListener("click", (event)=>{
+	copyBtn.innerText = "Copied! 🥳";
+	setTimeout(function(){
+		copyBtn.innerText = "Copy";},
+		1200);
     outputCode.select();
-    document.execCommand("copy");
-    alert("Code Copied To Clipboard");
+	document.execCommand("copy");
+	textarea.value = style;
+	document.execCommand('copy-btn', input.select());
+	copyButton.innerText = 'Copied';
+	setTimeout(()=>{
+    copyBtnButton.innerText = 'Copy';
+  }, 400);
 });
+
 generateBtn.addEventListener("click", generateGrad);
 window.onload = generateGrad;
